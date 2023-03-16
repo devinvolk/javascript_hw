e0dd69563a371975b858c3b05fee4342d2b67f3f
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-03-15 22:18:25 -0700

    auto generating list items for card to display the abilities

--- day3/homework3.js
+++ day3/homework3.js
@@ -1,28 +1,29 @@
+// setting up the submit buttons action
 const formData = document.querySelector('#form-data')
 console.log(formData)
 formData.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log(event)
     const name = formData.name.value
     pokemonData(name.toLowerCase())
+    abilityShow(name.toLowerCase())
 })
 
+// creating the async function to query the api and generate the card to display the data retrieved
 async function pokemonData(name){
     url = `https://pokeapi.co/api/v2/pokemon/${name}`
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data)
     const searchDiv = document.querySelector('#searchDiv')
     searchDiv.innerHTML = `
     <div class="card" style="width: 18rem;">
-        <img src="${data.sprites.front_shiny}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${data.name.toUpperCase()}</h5>
-    </div>
+      <img src="${data.sprites.front_shiny}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${data.name.toUpperCase()}</h5>
+      </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">Abilities:</li>
-      <li class="list-group-item">${data.abilities[0].ability.name}</li>
-      <li class="list-group-item">${data.abilities[1].ability.name}</li>
+      <div id="displayAbilities">
+      </div>
     </ul>
     <div class="card-body">
       <a href="#" class="card-link">Add to Team</a>
@@ -30,5 +31,12 @@ async function pokemonData(name){
     </div>
   </div>
   `
+  // creating a loop to generate the list item for the ability of the queried pokemon
+  // will display every ability
+    let display = ``
+    for (index in data.abilities){
+      display += `<li class="list-group-item">${data.abilities[index].ability.name}</li>`
+  }
+    // const displayAbilities = document.querySelector('#displayAbilities')
+    displayAbilities.innerHTML = display
 }
-
